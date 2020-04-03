f602f08038bdb7eb4ea3e90e912a9cf65b25ff14
Piero <pieromanrique@ucsp.edu.pe>
2020-04-02 20:31:15 -0600

    Second commit

---
+++ Tarea.cpp
@@ -0,0 +1,14 @@
+// Tarea.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int i;
+    std::cin >> i;
+    std::cout <<"Su numero es: "<< i;
+}
+
+
